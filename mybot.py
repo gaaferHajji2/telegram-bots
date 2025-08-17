ed601e30d391c46e89de0205d1fa588cb6185352
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-17 06:00:37 +0300

    Check user msg for hello and write the response

--- mybot.py
+++ mybot.py
@@ -12,8 +12,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}') # type: ignore
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_txt = update.message.text # type: ignore
-    await update.message.reply_text(f"Your text is: {user_txt}") # type: ignore
+    user_txt = update.message.text.lower() # type: ignore
+
+    if "hello" in  user_txt:
+        await update.message.reply_text("Hi, How I can help You") # type: ignore
+    else:
+        await update.message.reply_text(f"Your text is: {user_txt}") # type: ignore
 
 def main():
     TOKEN = os.environ.get('TOKEN', 'NULL');
