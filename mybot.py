fc633781300c9af34d99c065b3dd86dece74911f
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-17 05:54:48 +0300

    Adding Messagehandler for text only

--- mybot.py
+++ mybot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 from dotenv import load_dotenv
 
@@ -11,6 +11,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hello Telegram Bot by JLoka World") # type: ignore
     await update.message.reply_text(f'Hello {update.effective_user.first_name}') # type: ignore
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_txt = update.message.text # type: ignore
+    await update.message.reply_text(f"Your text is: {user_txt}") # type: ignore
+
 def main():
     TOKEN = os.environ.get('TOKEN', 'NULL');
 
@@ -19,6 +23,7 @@ def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start));
+    app.add_handler(MessageHandler(filters.TEXT & ~ filters.COMMAND, echo))
 
     app.run_polling()
 
