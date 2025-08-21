65cf273582b24d260187f5d2b4b7ae09580d63e9
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-21 22:02:54 +0300

    Adding try-catch to commands

--- mybot.py
+++ mybot.py
@@ -8,16 +8,23 @@ import os
 load_dotenv()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Hello Telegram Bot by JLoka World") # type: ignore
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}') # type: ignore
+    try:
+        await update.message.reply_text("Hello Telegram Bot by JLoka World") # type: ignore
+        await update.message.reply_text(f'Hello {update.effective_user.first_name}') # type: ignore
+    except Exception as e:
+        print(f"The Error msg of start-command is: {e}")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_txt = update.message.text.lower() # type: ignore
 
-    if "hello" in  user_txt:
-        await update.message.reply_text("Hi, How I can help You") # type: ignore
-    else:
-        await update.message.reply_text(f"Your text is: {user_txt}") # type: ignore
+    try:
+        user_txt = update.message.text.lower() # type: ignore
+
+        if "hello" in  user_txt:
+            await update.message.reply_text("Hi, How I can help You") # type: ignore
+        else:
+            await update.message.reply_text(f"Your text is: {user_txt}") # type: ignore
+    except Exception as e:
+        print(f"Error msg in echo-command, with msg: {e}")
 
 def main():
     TOKEN = os.environ.get('TOKEN', 'NULL');
