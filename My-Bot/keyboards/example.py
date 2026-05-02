efe41d7ce2fe52c6d5ddd4ffee1a93b34e017bf8
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 16:46:15 +0300

    Change the placeholder message of the keyboard of the bot

--- My-Bot/keyboards/example.py
+++ My-Bot/keyboards/example.py
@@ -7,4 +7,4 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 menu = ReplyKeyboardMarkup(keyboard=[
     [ KeyboardButton(text="Information") ], 
     [ KeyboardButton(text="Add Number"), KeyboardButton(text="Delete Number")]
-], resize_keyboard=True)
+], resize_keyboard=True, input_field_placeholder="JLoka Option To Verifying...")
