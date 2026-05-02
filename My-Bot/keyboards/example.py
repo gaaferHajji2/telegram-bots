fb8028176bafeb35d0662899402f846a8f6cee07
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 16:49:52 +0300

    Adding one time keyword option

--- My-Bot/keyboards/example.py
+++ My-Bot/keyboards/example.py
@@ -4,7 +4,11 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 # Each inner list represents a single row. For example, the first list contains all buttons 
 # for row 1, the second list for row 2, and so on.
 
-menu = ReplyKeyboardMarkup(keyboard=[
-    [ KeyboardButton(text="Information") ], 
-    [ KeyboardButton(text="Add Number"), KeyboardButton(text="Delete Number")]
-], resize_keyboard=True, input_field_placeholder="JLoka Option To Verifying...")
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [ KeyboardButton(text="Information") ], 
+        [ KeyboardButton(text="Add Number"), KeyboardButton(text="Delete Number")]
+    ], 
+    resize_keyboard=True, input_field_placeholder="JLoka Option To Verifying...", 
+    one_time_keyboard=True
+)
