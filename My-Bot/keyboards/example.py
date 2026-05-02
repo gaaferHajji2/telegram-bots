dc988b85bb4a836c5865311c1ace579500ce5bd1
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 16:28:55 +0300

    Adding comment about the reply keyboard markup

--- My-Bot/keyboards/example.py
+++ My-Bot/keyboards/example.py
@@ -1,3 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+#The keyboard argument in ReplyKeyboardMarkup accepts a matrix of buttons (a list of lists). 
+# Each inner list represents a single row. For example, the first list contains all buttons 
+# for row 1, the second list for row 2, and so on.
+
 menu = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Information")]])
