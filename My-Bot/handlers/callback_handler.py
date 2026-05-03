e85415f11c50319d65ad9e87d1e9c887f0aff5df
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-03 16:16:30 +0300

    Create the callback handler

---
+++ My-Bot/handlers/callback_handler.py
@@ -0,0 +1,11 @@
+from aiogram import F, Router
+
+router = Router()
+
+@router.callback_query(F.data == 'Russian/English/Arabic')
+async def get_languages(message):
+    await message.answer("My Languages are: \n1. Russian\n2. Arabic\n3. English")
+
+@router.callback_query(F.data == "Jafar Loka")
+async def get_my_name(message):
+    await message.answer("My Name is Jafar Loka.\nI am ITE Engineer & QA Tester")
