95f9701363d55b8b48f7852c07a3f813b585112f
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-09 13:13:30 +0300

    Refactor

--- My-Bot/handlers/callback_handler.py
+++ My-Bot/handlers/callback_handler.py
@@ -4,7 +4,7 @@ router = Router()
 
 @router.callback_query(F.data == 'Russian/English/Arabic')
 async def get_languages(message):
-    await message.answer("My Languages are: \n1. Russian\n2. Arabic\n3. English")
+    await message.answer("My Languages are: \n1. English\n2. Russian\n3. Arabic")
 
 @router.callback_query(F.data == "Jafar Loka")
 async def get_my_name(message):
