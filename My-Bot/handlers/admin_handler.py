ff418238c01217ee9bfaf0e895e754bad6d70ae4
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-10 15:00:28 +0300

    Adding the filter to the router message

--- My-Bot/handlers/admin_handler.py
+++ My-Bot/handlers/admin_handler.py
@@ -2,8 +2,14 @@ from aiogram import F, Router
 from filters.filter_admin import AdminFilter
 
 router = Router()
+router.message.filter(AdminFilter())
 
-@router.message(AdminFilter(), F.text == 'secret')
+@router.message(F.text == 'secret')
 async def admin_message(message):
     print(f"The user id is: {message.from_user.id}")
     await message.answer("This is secret message")
+
+@router.message(F.text == 'admin')
+async def admin_message(message):
+    print(f"The user id is: {message.from_user.id}")
+    await message.answer("This is admin message")
