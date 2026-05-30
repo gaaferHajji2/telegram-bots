b0115305f632a5256ff4497b9bab11a7a5cfb06e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-30 16:51:32 +0300

    Remove unused code and imports

--- aiogram-bot-01/handlers/start.py
+++ aiogram-bot-01/handlers/start.py
@@ -1,12 +1,18 @@
 from aiogram import Router
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import Command
 
 router = Router()
 
-@router.message(CommandStart())
-async def start(message):
-    await message.answer('We are glad to see you in our bot')
+@router.message(Command('start'))
+async def start(message, command):
+    if command.args == 'shop':
+        await message.answer('Welcome to the store menu')
+    elif command.args == 'feedback':
+        await message.answer('Here you can leave your feedback')
+    else:
+        await message.answer('This command is not supported. For more information about our commands, press /help')
 
 @router.message(Command('help'))
 async def help(message):
     await message.answer('For assistance, contact the administrator @jafarloka')
+
