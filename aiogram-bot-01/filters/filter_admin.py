20ec6a0d8fec3478be1d20e800f49682544a62bd
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-30 16:38:43 +0300

    Remove unused code

--- aiogram-bot-01/filters/filter_admin.py
+++ aiogram-bot-01/filters/filter_admin.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from aiogram.filters import Filter
 
 load_dotenv()
-# print(f"My Id is: {os.getenv('my_id')}")
 admins = [1046276866, 1234567890, int(os.getenv('my_id'))]
 
 class AdminFilter(Filter):
@@ -11,5 +10,4 @@ class AdminFilter(Filter):
         pass
 
     async def __call__(self, message):
-        # print(f"The user id is: {message.from_user.id}")
         return message.from_user.id in admins
